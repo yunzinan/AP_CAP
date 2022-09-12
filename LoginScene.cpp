88fb65c06d4bfc647adc1fc9e261e708099084b2
yunzinan <211850009@smail.nju.edu.cn>
2022-09-12 11:58:36 +0800

    [upd]用户信息读写

--- LoginScene.cpp
+++ LoginScene.cpp
@@ -1,11 +1,13 @@
 //
 // Created by Jack_shen on 2022/9/12.
 //
-
+#include <iostream>
+#include <fstream>
 #include "LoginScene.h"
 #include "LoginScene.h"
 
 LoginScene::LoginScene() {
+    this->loadUserInfo();
     printf("------------------------------------------------------------\n");
     printf("*****************************LOGIN**************************\n");
     printf("------------------------------------------------------------\n");
@@ -20,8 +22,31 @@ LoginScene::~LoginScene() {
 }
 
 void LoginScene::loadUserInfo() {
+    std::fstream fin("../res/userInfo.txt", std::ios::in);
+    if (!fin) {
+        printf("file not exists!\n");
+        std::fstream fout;
+        fout.open("../res/userInfo.txt", std::ios::out);
+        std::string head = "userID,username,password,phoneNumber,address,balance,userState";
+        fout << head << std::endl;
+        fout.close();
+    }
+    std::string buffer;
+    fin >> buffer; // 先去掉标题行
+    while(!fin.eof()) {
+        buffer.clear();
+        fin >>  buffer;
+        if(this->idx >= USERCAPACITY) {
+            printf("out of capacity!\n");
+        }
+        else {
+            this->userInfoList[this->idx++] = this->createUser(buffer);
+//            std::cout << this->userInfoList[this->idx-1]->username << this->userInfoList[this->idx-1]->password << std::endl;
+        }
 
+    }
 }
+
 
 void LoginScene::selectOpt() {
 
@@ -41,4 +66,42 @@ void LoginScene::adminLogin() {
 
 void LoginScene::selectUserOpt() {
 
+}
+
+userInfo* LoginScene::createUser(std::string &buffer) {
+    if(buffer == "") {
+        printf("error! no input!\n");
+        return NULL;
+    }
+    //create a new userInfo struct
+    userInfo * cur = new userInfo;
+    //split
+    int l = 0, r = 0;//双指针
+    //userID,
+    while(buffer[++r] != ',') {}
+    cur->userID = buffer.substr(l, r-l);
+    l = r+1;
+    //username,
+    while(buffer[++r] != ',') {}
+    cur->username = buffer.substr(l, r-l);
+    l = r+1;
+    //password,
+    while(buffer[++r] != ',') {}
+    cur->password = buffer.substr(l, r-l);
+    l = r+1;
+    //phonenumber,
+    while(buffer[++r] != ',') {}
+    cur->phonenumber = buffer.substr(l, r-l);
+    l= r+1;
+    //address,
+    while(buffer[++r] != ',') {}
+    cur->address = buffer.substr(l, r-l);
+    l = r+1;
+    //balance,
+    while(buffer[++r] != ',') {}
+    cur->balance = atof(buffer.substr(l, r-l).c_str());
+    l = r+1;
+    //userState
+    cur->userState = buffer.substr(l, buffer.length() - l);
+    return cur;
 }
--- LoginScene.h
+++ LoginScene.h
@@ -11,14 +11,15 @@
 #define USERCAPACITY 20
 
 typedef struct userInfo
-{
-    std::string userID; // U001
-    std::string userName; // no more than 10
-    std::string password; // no more than 20
-    std::string phonenumber; // no more than 40, numbers only
-    std::string address; // nomore than 40,
-    std::string userState; // activate, inactivate
 
+    {
+        std::string userID; // U001
+        std::string username; // no more than 10
+        std::string password; // no more than 20
+        std::string phonenumber; // no more than 40, numbers only
+        std::string address; // nomore than 40,
+        float balance; // setprecision 1
+        std::string userState; // activate, inactivate
 }userInfo;
 
 
@@ -26,6 +27,7 @@ class LoginScene
 {
 private:
     userInfo *userInfoList[USERCAPACITY];// 预设用户量上限为20
+    int idx = 0;
 public:
     LoginScene();
     void loadUserInfo(); //在初始化的时候从文件中读取信息或创建新文件
@@ -34,6 +36,7 @@ public:
     void userRegister();
     void adminLogin();
     void selectUserOpt(); // 用户中心选择显示和接收
+    userInfo* createUser(std::string &buffer); //接收完整的一行信息并创建一个用户结构体, 返回其地址
     ~LoginScene();
 };
 
